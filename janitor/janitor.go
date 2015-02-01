9af24d7369158a61edc327cdedfff9894fa4b3d4
Kim, Hirokuni <hirokuni.kim@kvh.co.jp>
2015-02-02 00:24:35 +0900

    wip

---
+++ janitor/janitor.go
@@ -0,0 +1,46 @@
+package janitor
+
+import (
+	"fmt"
+	"github.com/fsouza/go-dockerclient"
+	"os"
+	"time"
+)
+
+var (
+	client   *docker.Client
+	endpoint string
+)
+
+func init() {
+	endpoint = os.Getenv("DOCKER_HOST")
+
+	fmt.Println(endpoint)
+
+	if endpoint == "" {
+		panic("DOCKER_HOST is not set")
+	}
+}
+
+func Clean() {
+	timeLimit := int64(1000)
+	client, _ := docker.NewClient(endpoint)
+	currentTime := time.Now().Local().Unix()
+
+	option := docker.ListContainersOptions{}
+
+	containers, _ := client.ListContainers(option)
+
+	for _, container := range containers {
+		createdDate := container.Created
+		runningTime := currentTime - createdDate
+
+		if runningTime > timeLimit {
+			client.StopContainer(container.ID, 0)
+			fmt.Printf("Kill %s\n", container.ID)
+		}
+	}
+
+	fmt.Println("janitor is called!!!")
+
+}
